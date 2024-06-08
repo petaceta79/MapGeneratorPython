97befcf2e56ab1b2087e5c9813e207903fd20b81
petaceta79 <66634460+petaceta79@users.noreply.github.com>
2024-06-08 13:14:57 +0200

    Update genMapa.py
    No se eliminan als salas iniciales

--- genMapa.py
+++ genMapa.py
@@ -47,14 +47,14 @@ def EstaRodeadaSiONo(mapa, posicion):
             return False
     return True
 
-def eliminarRodeadas(mapa, posicion):
-    if EstaRodeadaSiONo(mapa, posicion) and random.random() < porcentageEliminarRodeadas:
-        return False
-    else:
+def eliminarRodeadas(mapa, posicion, porcentageEliminarRodeadas, casillasEspeciales):
+    if EstaRodeadaSiONo(mapa, posicion) and random.random() < porcentageEliminarRodeadas and mapa[posicion[0]][posicion[1]] not in casillasEspeciales:
         return True
+    else:
+        return False
     
 # Ampliar el mapa
-def ampliarMapa(mapa, NumeroCasillas, tiposDeHabitaciones):
+def ampliarMapa(mapa, NumeroCasillas, tiposDeHabitaciones, porcentageEliminarRodeadas, casillasEspeciales):
     if(NumeroCasillas+1 <= (len(mapa)*len(mapa))): # Comprobar que no pida mas casillas de las posibles
         while contadorSalas(mapa) < NumeroCasillas+1:
             posicionACambiar = random.choice(posicionDeCasillas(mapa))
@@ -68,49 +68,53 @@ def ampliarMapa(mapa, NumeroCasillas, tiposDeHabitaciones):
             if(direccion == 4 and posicionACambiar[1] != 0 and mapa[posicionACambiar[0]][posicionACambiar[1]-1] == 0): # Izquierda
                 mapa[posicionACambiar[0]][posicionACambiar[1]-1] = random.choice(tiposDeHabitaciones)
         for e in posicionDeCasillas(mapa):
-            if not(eliminarRodeadas(mapa, e)):
+            if(eliminarRodeadas(mapa, e, porcentageEliminarRodeadas, casillasEspeciales)):
                 mapa[e[0]][e[1]] = 0
     return mapa
 
-# Empieza codigo 
-tamanoDelMapa = 15
-numeroDeHabitacionesIntervalo = [70,80]
-numeroDeHabitaciones = random.randint(numeroDeHabitacionesIntervalo[0],numeroDeHabitacionesIntervalo[1])
-tiposDeHabitaciones = [2,3,4,5,6]
-porcentageEliminarRodeadas = 0.8
+# Crear el mapa
+def crearMapa(tamanoDelMapa = 15, numeroDeHabitacionesIntervalo = [70,80], tiposDeHabitaciones = [2,3,4,5,6], porcentageEliminarRodeadas = 0.8, casillasEspeciales = [1]):
+    # TamanoDelMapa = 15 lo que el mapa medira de largo y ancho 
+    # NumeroDeHabitacionesIntervalo intervalo de habitaciones posibles
+    numeroDeHabitaciones = random.randint(numeroDeHabitacionesIntervalo[0],numeroDeHabitacionesIntervalo[1]) # Cantidad de habitaciones
+    # TiposDeHabitaciones tipos de habitaciones posibles (normales)
+    # PorcentageEliminarRodeadas porcentage de casillas rodeadas que se eliminan
+    # casillasEspeciales son las casillas como por ejemplo el 1 (el inicio) para no ser eliminadas y delimitar su ratio de spawn
 
-mapa = generarArrayMapa(tamanoDelMapa-1)
-mapa[int(len(mapa)/2)][int(len(mapa[0])/2)] = 1 # La primera habitacion en el centro
+    mapa = generarArrayMapa(tamanoDelMapa-1)
+    mapa[int(len(mapa)/2)][int(len(mapa[0])/2)] = 1 # La primera habitacion en el centro
 
-mapa = ampliarMapa(mapa, numeroDeHabitaciones-1, tiposDeHabitaciones)
+    mapa = ampliarMapa(mapa, numeroDeHabitaciones-1, tiposDeHabitaciones, porcentageEliminarRodeadas, casillasEspeciales)
 
-verMapa(mapa)
+    return mapa
 
-# Crear imagen para ver el mapa ------------------------------------
-from PIL import Image, ImageDraw, ImageFont
+def verMapaPNG(mapa):
+    # Crear imagen para ver el mapa ------------------------------------
+    from PIL import Image, ImageDraw, ImageFont
 
-# Crear fondo
-ancho, alto = 50*len(mapa), 50*len(mapa)
-imagen = Image.new('RGB', (ancho, alto), 'white')
+    # Crear fondo
+    ancho, alto = 50*len(mapa), 50*len(mapa)
+    imagen = Image.new('RGB', (ancho, alto), 'white')
 
-# Pintar mapa
-dibujo = ImageDraw.Draw(imagen)
-fuente = ImageFont.load_default()
+    # Pintar mapa
+    dibujo = ImageDraw.Draw(imagen)
+    fuente = ImageFont.load_default()
 
-for i in range(len(mapa)):
-    for e in range(len(mapa[i])):
-        if mapa[i][e] != 0:
-            if mapa[i][e] == 1:
-                dibujo.rectangle([i*50, e*50, i*50+50, e*50+50], outline='black', fill='green')
-                dibujo.text((i*50 + 25, e*50 + 25), str(mapa[i][e]), font=fuente, fill='black')
+    for i in range(len(mapa)):
+        for e in range(len(mapa[i])):
+            if mapa[i][e] != 0:
+                if mapa[i][e] == 1:
+                    dibujo.rectangle([i*50, e*50, i*50+50, e*50+50], outline='black', fill='green')
+                    dibujo.text((i*50 + 25, e*50 + 25), str(mapa[i][e]), font=fuente, fill='black')
+                else:
+                    dibujo.rectangle([i*50, e*50, i*50+50, e*50+50], outline='black', fill='blue')
+                    dibujo.text((i*50 + 25, e*50 + 25), str(mapa[i][e]), font=fuente, fill='black')
             else:
-                dibujo.rectangle([i*50, e*50, i*50+50, e*50+50], outline='black', fill='blue')
-                dibujo.text((i*50 + 25, e*50 + 25), str(mapa[i][e]), font=fuente, fill='black')
-        else:
-            dibujo.rectangle([i*50, e*50, i*50+50, e*50+50], outline='black', fill='white')
+                dibujo.rectangle([i*50, e*50, i*50+50, e*50+50], outline='black', fill='white')
+    # Guardar imagen
+    imagen.save('MapaFoto.png')
+    imagen.show()
 
-
-
-# Guardar imagen
-imagen.save('MapaFoto.png')
-imagen.show()
+#mapa = crearMapa(35, [500,500], [2,3,4,5,6], 0.65, [1])
+#verMapa(mapa)
+#verMapaPNG(mapa)
